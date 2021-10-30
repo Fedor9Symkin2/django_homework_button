e088fc17861305d44a1fc0470070988dc47d7cf9
Nikolay Savenko <nikolay.savenko.92@yandex.ru>
2021-10-30 11:54:32 +0300

    create basee and tests

--- pages/urls.py
+++ pages/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 
-from .views import HomePageVies
+from .views import HomePageVies, AboutPageView
 
 urlpatterns = [
+    path('about/', AboutPageView.as_view(), name='about'),
     path('', HomePageVies.as_view(), name='home'),
 ]
